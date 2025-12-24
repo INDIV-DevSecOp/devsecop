31fad8b0f5b44d57bef0aa2792c136df89402965
Thanakrit_B <thanakirt47@gmail.com>
2025-12-24 23:55:23 +0700

    Add terraform EC2 creating

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,51 +1,102 @@
+# Provider Configuration
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
 
-# Configure the AWS Provider
+# Configuration the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-# create security group for the ec2 instance
-resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2 security group"
-  description = "allow access on ports 22"
+# Data source for latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
 
-  # allow access on port 22
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Security Group Resource
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow HTTP and SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
   ingress {
-    description = "ssh access"
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
-    protocol    = -1
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Monitoring server security group"
+    Name = "allow_http_ssh"
   }
+
 }
 
+# EC2 Instance Resource
 resource "aws_instance" "Monitoring_server" {
-ami = "ami-00bb6a80f01f03502"  
-instance_type = "t2.medium"
-security_groups = [aws_security_group.ec2_security_group.name]
-key_name = var.key_name
-tags = {
-  Name: var.instance_name
-}
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = var.instance_type
+  key_name      = var.key_pair
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "Hello, World from $(hostname -f)" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Monitoring-Server"
+  }
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,17 +1,29 @@
-variable "instance_name" {
-    default = "Monitoring_server"  # Names of the instance
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-southeast-1"
 }
 
-variable "key_name" {
-  default = "terra"                  # Names of key in aws
+variable "instance_type" {
+  description = "The type of AWS EC2 instance to use"
+  type        = string
+  default     = "t2.micro"
 }
-
 
 variable "access_key" {
-  default = "insert"                # aws access key
+  description = "The AWS access key"
+  type        = string
+  sensitive   = true
 }
 
 variable "secret_key" {
-  default = "insert"         # aws secret key
+  description = "The AWS secret key"
+  type        = string
+  sensitive   = true
 }
 
+variable "key_pair" {
+  description = "The name of the SSH key pair"
+  type        = string
+  default     = "my-key-pair"
+}
